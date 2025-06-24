2f4e553d9d2860d18bf237c570c50d4f8b52a608
IsaacVilla11 <124637331+IsaacVilla11@users.noreply.github.com>
2025-06-23 20:36:41 -0500

    first commit

---
+++ src/main/java/com/facturacion/facturacionback/FacturacionBackApplication.java
@@ -0,0 +1,13 @@
+package com.facturacion.facturacionback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturacionBackApplication.class, args);
+	}
+
+}
